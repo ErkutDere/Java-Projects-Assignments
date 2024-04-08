47a7c071eb806779585e016bdffe2d305475ed19
ErkutDere <114229232+ErkutDere@users.noreply.github.com>
2024-04-08 21:38:07 +0300

    Add files via upload

---
+++ homework1/Interaction.java
@@ -0,0 +1,27 @@
+package homework1;
+public abstract class Interaction {
+    final int interactionId;
+    // final int accountId;
+    // final int postId;
+    final Account account;
+    final Post post;
+/**
+ * This is the constructor of abstract class interaction
+ * @param interactionId
+ * @param a
+ * @param p
+ */
+    public Interaction(int interactionId, Account a, Post p) {
+        this.interactionId = interactionId;
+        this.account = a;
+        this.post = p;
+    }
+/**
+ * This method returns the username of an account. This information used for viewInteraction method.
+ * @return username of the account
+ */
+    public String getUsername() {
+        return account.getUserName();
+    }
+
+}
---
+++ homework1/Like.java
@@ -0,0 +1,21 @@
+package homework1;
+public class Like extends Interaction {
+    /**
+     * This subclass extends interaction class and uses it's parent constructor.
+     * @param interactionId
+     * @param a
+     * @param p
+     */
+    public Like(int interactionId, Account a, Post p) {
+        super(interactionId, a, p);
+        // TODO Auto-generated constructor stub
+    }
+/**
+ * This method used for viewing post's likes account's. It returns account object.
+ * @return Account object
+ */
+    public Account likeOwner() {
+        return super.account;
+    }
+
+}
